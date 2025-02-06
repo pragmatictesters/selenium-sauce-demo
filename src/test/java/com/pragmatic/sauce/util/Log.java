2b03413112283817f3cab5dae4b177ee6ad252f1
Janesh Kodikara <janesh.kodikara@gmail.com>
2025-02-06 21:59:10 +0530

    Add additional log methods to Log class to incorporate parameters into the log messages

--- src/test/java/com/pragmatic/sauce/util/Log.java
+++ src/test/java/com/pragmatic/sauce/util/Log.java
@@ -18,9 +18,19 @@ public class Log {
         logger.info(message);
     }
 
+    public static void info(String message, String parameter) {
+        logger.info(message, parameter);
+    }
+
     public static void warn(String message) {
         logger.warn(message);
     }
+
+    public static void warn(String message, String parameter) {
+        logger.warn(message, parameter);
+    }
+
+
     public static void error(String message) {
         logger.error(message);
     }
